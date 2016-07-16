9388189da2ef8248b8ba1fd89003739c2119a9ac
d.takeuchi <daiki.takeuchi.0327@gmail.com>
2016-07-16 10:43:40 +0900

    phpdocumentor/reflectionが>=php5.5になっているため、TravisCIでのテストをしないように修正

--- application/config/config.php
+++ application/config/config.php
@@ -314,7 +314,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '09aa923139ed638ac2480d1c91aa8bf2';
 
 /*
 |--------------------------------------------------------------------------
