5de6637f4c95ef689153cde1280471975a84af1d
d.takeuchi <daiki.takeuchi.0327@gmail.com>
2016-08-12 05:29:39 +0900

    pagerのカラム落ちを修正

--- application/config/pagination.php
+++ application/config/pagination.php
@@ -2,11 +2,11 @@
 //$config['reuse_query_string'] = TRUE;
 $config['full_tag_open'] = '<ul class="pager">';
 $config['full_tag_close'] = '</ul>';
-$config['num_links'] = 3;
-$config['prev_link'] = '&lt; 前<span class="spNone1024">のページ</span>';
+$config['num_links'] = 2;
+$config['prev_link'] = '&lt;<span class="spNone1024"> 前のページ</span>';
 $config['prev_tag_open'] = '<li class="previous">';
 $config['prev_tag_close'] = '</li>';
-$config['next_link'] = '次<span class="spNone1024">のページ</span> &gt;';
+$config['next_link'] = '<span class="spNone1024">次のページ </span>&gt;';
 $config['next_tag_open'] = '<li class="next">';
 $config['next_tag_close'] = '</li>';
 $config['cur_tag_open'] = '<li class="active"><a href="#">';
