449c9156bf6f26f9d7ebf40844a4e2ebd2e76958
d.takeuchi <daiki.takeuchi.0327@gmail.com>
2016-10-16 22:23:19 +0900

    開始日、終了日を見るように修正

--- application/models/Organization_model.php
+++ application/models/Organization_model.php
@@ -17,10 +17,11 @@ class Organization_model extends MY_Model
         return $this->find();
     }
 
-    public function get_organization_for_select($company_cc, $start_date) {
+    public function get_organization_for_select($company_cc, $date) {
         $this->db->select("company_cc || '|' || soshiki_cc soshiki_cc , soshiki_nm");
         $this->db->where("company_cc",strval($company_cc));
-        $this->db->where("start_date <=",$start_date);
+        $this->db->where("start_date <=",$date);
+        $this->db->where("end_date >=",$date);
         $this->db->order_by("soshiki_cc");
         return $this->find();
     }
